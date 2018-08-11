e741c18280c92b0f4c6021d2dbe7cec6512f38a1
packiasheelav <packiasheelav@gmail.com>
2018-08-12 00:24:50 +0300

    newPost Modified

--- src/component/AddNewPost.js
+++ src/component/AddNewPost.js
@@ -8,11 +8,11 @@ import {
       <form onSubmit = {e => (
             e.preventDefault(),
             props.change(e))} >
-      <label className='labelBox' >Title</label>
-      <input className='inputBoxes'type="text" placeholder="Title" name='post' />
+      <label className='title' >Title</label>
+      <input className='inputBoxes'type="text" placeholder="Title" name='post' /><br /><br />
       <label className='labelBox'>Category</label>
-      <input className='inputBoxes' name='category'/><br/>
-      <label className='labelBox'>Write New Post</label>
+      <input className='inputBoxes' name='category'/><br/><br />
+      <label className='labelBox'>your Message here</label>
       <textarea  className='inputBoxforTextArea' name='postContent' type='text'/><br/>
       <input type='submit' value='save' />
       <button > <li className='AddNewPost'><NavLink exact activeStyle ={{color:'orange'}} to="/" >Cancel</NavLink></li></button>
